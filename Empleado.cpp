42fd847a842a2b14b8d26873c091f6c4c1534bca
Byron Jimenez <byronjimenez9911@gmail.com>
2023-06-19 19:41:58 -0600

    Create Empleado.cpp

---
+++ Empleado.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Empleado {
+private:
+    string nombre;
+    string direccion;
+    string fechaNacimiento;
+    char sexo;
+
+public:
+    Empleado(string nombre, string direccion, string fechaNacimiento, char sexo) {
+        this->nombre = nombre;
+        this->direccion = direccion;
+        this->fechaNacimiento = fechaNacimiento;
+        this->sexo = sexo;
+    }
+
+    string getNombre() {
+        return nombre;
+    }
+
+    string getDireccion() {
+        return direccion;
+    }
+
+    string getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    char getSexo() {
+        return sexo;
+    }
+};
