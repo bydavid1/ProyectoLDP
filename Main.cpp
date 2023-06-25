dc456189c8b49292cb5cdb8361c0c8783a1931f0
Byron Jimenez <byronjimenez9911@gmail.com>
2023-06-25 14:57:43 -0600

    Configuracion de lanzamiento con g++ agregado

--- Main.cpp
+++ Main.cpp
@@ -9,14 +9,17 @@
 
 using namespace std;
 
+void ordenarEmpleadosPorApellido(vector<Empleado *> &empleados);
+void ordenarEmpleadosPorSueldo(vector<Empleado *> &empleados);
+void mostrarCantidadEmpleadosSegunRoles(const vector<Empleado *> &empleados);
+void agregarNuevoEmpleado(vector<Empleado *> &empleados);
+void generarEmpleadosPrueba(vector<Empleado *> &empleados);
+
 int main()
 {
     vector<Empleado *> empleados;
-    int contadorGerentes = 0;
-    int contadorJefesArea = 0;
-    int contadorSupervisores = 0;
-    int contadorTecnicos = 0;
 
+    generarEmpleadosPrueba(empleados);
 
     int opcion;
     do
@@ -33,123 +36,141 @@ int main()
         switch (opcion)
         {
         case 1:
-            // Ordenar empleados por apellido
-            sort(empleados.begin(), empleados.end(), [](Empleado *emp1, Empleado *emp2)
-                 { return emp1->getNombre() < emp2->getNombre(); });
+            ordenarEmpleadosPorApellido(empleados);
             break;
         case 2:
-                    // Ordenar empleados por sueldo
-                sort(empleados.begin(), empleados.end(), [](const Empleado *emp1, const Empleado *emp2)
-                 { return emp1->calcularSalario() < emp2->calcularSalario(); });
-                  break;
-             case 3:
-
-             for (Empleado *emp : empleados)
-             {
-                if (dynamic_cast<Gerente *>(emp) != nullptr)
-                {
-                    contadorGerentes++;
-                }
-                else if (dynamic_cast<JefeArea *>(emp) != nullptr)
-                {
-                    contadorJefesArea++;
-                }
-                else if (dynamic_cast<Supervisor *>(emp) != nullptr)
-                {
-                    contadorSupervisores++;
-                }
-                else if (dynamic_cast<Tecnico *>(emp) != nullptr)
-                {
-                    contadorTecnicos++;
-                }
-            }
-
-            cout << "Cantidad de empleados según roles:" << endl;
-            cout << "Gerentes: " << contadorGerentes << endl;
-            cout << "Jefes de Área: " << contadorJefesArea << endl;
-            cout << "Supervisores: " << contadorSupervisores << endl;
-            cout << "Técnicos: " << contadorTecnicos << endl;
+            ordenarEmpleadosPorSueldo(empleados);
+            break;
+        case 3:
+            mostrarCantidadEmpleadosSegunRoles(empleados);
             break;
         case 4:
-        
-            string nombre, direccion, fechaNacimiento;
-            char sexo;
-            int Sueldo;
-
-            cout << "Agregar nuevo empleado" << endl;
-            cout << "Nombre: ";
-            cin >> nombre;
-            cout << "Dirección: ";
-            cin >> direccion;
-            cout << "Fecha de Nacimiento: ";
-            cin >> fechaNacimiento;
-            cout << "Sexo: ";
-            cin >> sexo;
-            cout << "Sueldo: ";
-            cin >> Sueldo;
-
-            // Agregar nuevo empleado al vector empleados
-            // Puedes implementar la lógica para crear la instancia del empleado correspondiente según su rol y agregarlo al vector
-             // Solicitar información adicional según el rol del empleado
-
-         string rol;
-         cout << "Rol (Gerente/JefeArea/Supervisor/Tecnico): ";
-         cin >> rol;
-
-         if (rol == "Gerente.h") {
-            string sucursal;
-            cout << "sucursal: ";
-            cin >> sucursal;
-
-            // Crear instancia de Gerente y agregarlo al vector
-          Gerente* nuevoGerente = new Gerente(nombre, direccion, fechaNacimiento, sexo);
-             empleados.push_back(nuevoGerente);
-
-           } else if (rol == "JefeArea") {
-            string Area;
-            cout << "Nombre Area: ";
-            cin >> Area;
-
-            // Crear instancia de JefeArea y agregarlo al vector
-           JefeArea* nuevoJefeArea = new JefeArea(nombre, direccion, fechaNacimiento, sexo);
-          empleados.push_back(nuevoJefeArea);
-
-         }else if (rol == "Supervisor") {
-            string departamento;
-            cout << "Departamento: ";
-            cin >> departamento;
-
-         // Crear instancia de Supervisor y agregarlo al vector
-                
-            Supervisor *nuevoSupervisor = new Supervisor(nombre, direccion, fechaNacimiento, sexo);
-           empleados.push_back(nuevoSupervisor);
-
-            }else if (rol == "Tecnico") {
-            
-           
-            string campo;
-            cout << "Nombre del campo: ";
-            cin >> campo;
-
-                // Crear instancia de Tecnico y agregarlo al vector
-                Tecnico* nuevoTecnico = new Tecnico(nombre, direccion, fechaNacimiento, sexo);
-                 empleados.push_back(nuevoTecnico);
-
-            } else {
-          
-            
-                cout << "Rol no válido. No se agregó ningún empleado." << endl;
-            }
+            agregarNuevoEmpleado(empleados);
             break;
         }
 
     } while (opcion != 0);
 
-    // Liberar memoria de los empleados
+    return 0;
+}
+
+void ordenarEmpleadosPorApellido(vector<Empleado *> &empleados)
+{
+    sort(empleados.begin(), empleados.end(), [](Empleado *emp1, Empleado *emp2)
+         { return emp1->getNombre() < emp2->getNombre(); });
+
+    cout << "Empleados ordenados por apellido:" << endl;
     for (Empleado *emp : empleados)
     {
-        delete emp;
+        cout << emp->getNombre() << endl;
+    }
+}
+
+void ordenarEmpleadosPorSueldo(vector<Empleado *> &empleados)
+{
+    sort(empleados.begin(), empleados.end(), [](Empleado *emp1, Empleado *emp2)
+         { return emp1->calcularSalario() < emp2->calcularSalario(); });
+
+    // Mostrar la lista ordenada de empleados
+    cout << "Empleados ordenados por sueldo:" << endl;
+    for (Empleado *emp : empleados)
+    {
+        cout << emp->getNombre() << ": " << emp->calcularSalario() << endl;
+    }
+}
+
+void mostrarCantidadEmpleadosSegunRoles(const vector<Empleado *> &empleados)
+{
+    int contadorGerentes = 0;
+    int contadorJefesArea = 0;
+    int contadorSupervisores = 0;
+    int contadorTecnicos = 0;
+
+    for (Empleado *emp : empleados)
+    {
+        if (dynamic_cast<Gerente *>(emp) != nullptr)
+        {
+            contadorGerentes++;
+        }
+        else if (dynamic_cast<JefeArea *>(emp) != nullptr)
+        {
+            contadorJefesArea++;
+        }
+        else if (dynamic_cast<Supervisor *>(emp) != nullptr)
+        {
+            contadorSupervisores++;
+        }
+        else if (dynamic_cast<Tecnico *>(emp) != nullptr)
+        {
+            contadorTecnicos++;
+        }
     }
 
-    return 0;
-} 
+    cout << "Cantidad de empleados según roles:" << endl;
+    cout << "Gerentes: " << contadorGerentes << endl;
+    cout << "Jefes de Área: " << contadorJefesArea << endl;
+    cout << "Supervisores: " << contadorSupervisores << endl;
+    cout << "Técnicos: " << contadorTecnicos << endl;
+}
+
+void agregarNuevoEmpleado(vector<Empleado *> &empleados)
+{
+    string nombre, direccion, fechaNacimiento;
+    char sexo;
+
+    cout << "Agregar nuevo empleado" << endl;
+    cout << "Nombre: ";
+    cin >> nombre;
+    cout << "Dirección: ";
+    cin >> direccion;
+    cout << "Fecha de Nacimiento: ";
+    cin >> fechaNacimiento;
+    cout << "Sexo: ";
+    cin >> sexo;
+
+    string rol;
+    cout << "Rol (Gerente/JefeArea/Supervisor/Tecnico): ";
+    cin >> rol;
+
+    if (rol == "Gerente")
+    {
+        Gerente *nuevoGerente = new Gerente(nombre, direccion, fechaNacimiento, sexo);
+        empleados.push_back(nuevoGerente);
+    }
+    else if (rol == "JefeArea")
+    {
+        JefeArea *nuevoJefeArea = new JefeArea(nombre, direccion, fechaNacimiento, sexo);
+        empleados.push_back(nuevoJefeArea);
+    }
+    else if (rol == "Supervisor")
+    {
+        Supervisor *nuevoSupervisor = new Supervisor(nombre, direccion, fechaNacimiento, sexo);
+        empleados.push_back(nuevoSupervisor);
+    }
+    else if (rol == "Tecnico")
+    {
+        Tecnico *nuevoTecnico = new Tecnico(nombre, direccion, fechaNacimiento, sexo);
+        empleados.push_back(nuevoTecnico);
+    }
+    else
+    {
+        cout << "Rol inválido. No se agrega empleado." << endl;
+    }
+}
+
+void generarEmpleadosPrueba(vector<Empleado *> &empleados)
+{
+    // Crear empleados de prueba
+    Empleado *empleado1 = new Gerente("John Doe", "123 Main St", "01/01/1990", 'M');
+    Empleado *empleado2 = new JefeArea("Jane Smith", "456 Elm St", "02/02/1991", 'F');
+    Empleado *empleado3 = new Supervisor("Mike Johnson", "789 Oak St", "03/03/1992", 'M');
+    Empleado *empleado4 = new Tecnico("Emily Davis", "987 Pine St", "04/04/1993", 'F');
+    Empleado *empleado5 = new Gerente("Robert Brown", "321 Maple St", "05/05/1994", 'M');
+
+    // Agregar empleados al vector
+    empleados.push_back(empleado1);
+    empleados.push_back(empleado2);
+    empleados.push_back(empleado3);
+    empleados.push_back(empleado4);
+    empleados.push_back(empleado5);
+}
